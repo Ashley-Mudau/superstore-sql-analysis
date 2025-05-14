b0bc5b17be2c2f98296c76ae7f105ad76c099db3
Ashley-Mudau <Ashleymuudau@gmail.com>
2025-05-14 21:56:37 +0200

    Add files via upload

---
+++ Superstore_SQL_Analysis.sql
@@ -0,0 +1,101 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 17.0 (Debian 17.0-1.pgdg120+1)
+-- Dumped by pg_dump version 17.0 (Debian 17.0-1.pgdg120+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: demo; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.demo (
+    id integer NOT NULL,
+    name character varying(200) DEFAULT ''::character varying NOT NULL,
+    hint text DEFAULT ''::text NOT NULL
+);
+
+
+--
+-- Name: demo_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.demo_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: demo_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.demo_id_seq OWNED BY public.demo.id;
+
+
+--
+-- Name: superstore; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.superstore (
+    order_id character varying,
+    order_date date,
+    ship_date date,
+    ship_mode character varying,
+    customer_id character varying,
+    customer_name character varying,
+    segment character varying,
+    country character varying,
+    city character varying,
+    state character varying,
+    postal_code character varying,
+    region character varying,
+    product_id character varying,
+    category character varying,
+    sub_category character varying,
+    product_name text,
+    sales numeric,
+    quantity integer,
+    discount numeric,
+    profit numeric
+);
+
+
+--
+-- Name: demo id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.demo ALTER COLUMN id SET DEFAULT nextval('public.demo_id_seq'::regclass);
+
+
+--
+-- Name: demo demo_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.demo
+    ADD CONSTRAINT demo_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
